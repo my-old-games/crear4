023316a368eefb629f03767579c48f5f7ce2dec2
Gastón Caminiti <gastoncaminiti@outlook.com>
2021-08-15 20:29:07 -0300

    9º Prototipo [JUGABLE - PARTE 3]

--- Proyecto/scripts/interfaces/Inicio.gd
+++ Proyecto/scripts/interfaces/Inicio.gd
@@ -2,18 +2,22 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite.play("ESPERANZA")
 
 func _physics_process(delta):
 	if $Jugar.is_hovered():
+		$AnimatedSprite.play("JUGAR")
 		$Jugar/Label.margin_top = 20
 	else:
 		$Jugar/Label.margin_top = 15
 	
 	if $Salir.is_hovered():
+		$AnimatedSprite.play("SALIR")
 		$Salir/Label.margin_top = 20
 	else:
 		$Salir/Label.margin_top = 15
+	if !$Jugar.is_hovered() and !$Salir.is_hovered():
+		$AnimatedSprite.play("ESPERANZA")
 
 func _on_Jugar_pressed():
 	get_tree().change_scene("res://niveles/Nivel01.tscn")
